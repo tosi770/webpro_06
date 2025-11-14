9d8681a6f45ce43be1e8541e7240c44779d174d4
tosi <tosi31429180@gmail.com>
2025-11-14 12:01:29 +0900

    京葉システムの追加

--- app5.js
+++ app5.js
@@ -1,8 +1,51 @@
+"use strict";
 const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
+
+let station = [
+  { id:1, code:"JE01", name:"東京駅"},
+  { id:2, code:"JE07", name:"舞浜駅"},
+  { id:3, code:"JE12", name:"新習志野駅"},
+  { id:4, code:"JE13", name:"幕張豊砂駅"},
+  { id:5, code:"JE14", name:"海浜幕張駅"},
+  { id:6, code:"JE05", name:"新浦安駅"},
+];
+
+let station2 = [
+  { id:1, code:"JE01", name:"東京駅", change:"総武本線，中央線，etc", passengers:403831, distance:0 },
+  { id:2, code:"JE02", name:"八丁堀駅", change:"日比谷線", passengers:31071, distance:1.2 },
+  { id:3, code:"JE05", name:"新木場駅", change:"有楽町線，りんかい線", passengers:67206, distance:7.4 },
+  { id:4, code:"JE07", name:"舞浜駅", change:"舞浜リゾートライン", passengers:76156,distance:12.7 },
+  { id:5, code:"JE12", name:"新習志野駅", change:"", passengers:11655, distance:28.3 },
+  { id:6, code:"JE17", name:"千葉みなと駅", change:"千葉都市モノレール", passengers:16602, distance:39.0 },
+  { id:7, code:"JE18", name:"蘇我駅", change:"内房線，外房線", passengers:31328, distance:43.0 },
+];
+
+app.get("/keiyo", (req, res) => {
+  res.render('db1', { data: station });
+});
+
+app.get("/keiyo2", (req, res) => {
+  res.render('keiyo2', { data: station2 });
+});
+
+app.get("/keiyo2/:number", (req, res) => {
+  const number = req.params.number;
+  const detail = station2[ number ];
+  res.render('keiyo2_detail', {data: detail} );
+});
+
+app.get("/keiyo_add", (req, res)=> {
+  let id = req.query.id;
+  let code = req.query.code;
+  let name = req.query.name;
+  let newdata = { id: id, code: code, name: name };
+  station.push( newdata );
+  res.redirect('/public/keiyo_add.html');
+});
 
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
@@ -37,24 +80,51 @@ app.get("/omikuji2", (req, res) => {
 });
 
 app.get("/janken", (req, res) => {
-  let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
-  let cpu = '';
-  let judgement = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
-  else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 以下の数行は人間の勝ちの場合の処理なので，
-  // 判定に沿ってあいこと負けの処理を追加する
-  judgement = '勝ち';
-  win += 1;
-  total += 1;
+  const isSubmitted = req.query.hand !== undefined;
+  let hand, win, total, num, cpu, judgement, yourHandStr;
+
+  if (isSubmitted) {
+    hand = Number(req.query.hand);
+    win = Number(req.query.win);
+    total = Number(req.query.total);
+
+    console.log( {hand, win, total});
+
+    num = Math.floor(Math.random() * 3 + 1);
+    cpu = '';
+    judgement = '';
+    if( num==1 ) cpu = 'グー';
+    else if( num==2 ) cpu = 'チョキ';
+    else cpu = 'パー';
+
+    if( hand==1 ) yourHandStr = 'グー';
+    else if( hand==2 ) yourHandStr = 'チョキ';
+    else yourHandStr = 'パー';
+    
+    // 勝敗判定
+    if(( hand==1 && num==2)||(hand==2 && num==3)||(hand==3 && num==1)){
+      judgement = '勝ち';
+      win += 1;
+      total += 1;
+    }
+    else if(( hand==1 && num==1)||(hand==2 && num==2)||(hand==3 && num==3)){
+      judgement = 'あいこ';
+    }
+    else{
+      judgement = '負け';
+      total += 1;
+    }
+
+  } else {
+    hand = undefined;
+    win = 0;
+    total = 0;
+    cpu = '';
+    judgement = '';
+    yourHandStr = '';
+  }
   const display = {
-    your: hand,
+    your: yourHandStr,
     cpu: cpu,
     judgement: judgement,
     win: win,
